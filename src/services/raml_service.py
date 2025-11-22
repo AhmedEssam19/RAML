32e8740b0687f17e4cb8bb610c735c90c9acee86
Ahmed <ahmed.elhlwgy@gmail.com>
2025-11-21 18:29:21 -0700

    Add email functionality

--- src/services/raml_service.py
+++ src/services/raml_service.py
@@ -1,15 +1,17 @@
 import os
 import tempfile
 import subprocess
+import smtplib
 
 import xml.etree.ElementTree as ET
 
+from email.mime.text import MIMEText
 from .raml.main import SmaliMalwareAnalyzer
 from ..celery import schedule_task
 
 
 @schedule_task
-async def analyze_apk_with_raml(apk_path: str):
+async def analyze_apk_with_raml(apk_path: str, user_email: str = "ahmed.mohamed8@ucalgary.ca"):
     smali_output = tempfile.mkdtemp(prefix="smali_code")
     decode_cmd = [
         "apktool",
@@ -22,15 +24,22 @@ async def analyze_apk_with_raml(apk_path: str):
 
     try:
         subprocess.run(decode_cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        package_name = get_package_name(smali_output)
+        analyzer = SmaliMalwareAnalyzer(smali_folder=smali_output, package_name=package_name)
+        await analyzer.setup_system(force_rebuild=False)
+        result = await analyzer.analyze_behaviors(list(range(1, 13)))
+        # send_email(user_email, "Your request is complete. Jump to the dashboard to view it!")
+        return result
+
     except subprocess.CalledProcessError as e:
+        # send_email(user_email, "Bad Luck! Your request failed")
         print(f"Command failed with exit code {e.returncode}:")
         print(f"STDOUT: {e.stdout}")
         print(f"STDERR: {e.stderr}")
     
-    package_name = get_package_name(smali_output)
-    analyzer = SmaliMalwareAnalyzer(smali_folder=smali_output, package_name=package_name)
-    await analyzer.setup_system(force_rebuild=False)
-    return await analyzer.analyze_behaviors(list(range(1, 13)))
+    except Exception as e:
+        # send_email(user_email, "Bad Luck! Your request failed")
+        pass
 
 
 def get_package_name(apktool_output_dir):
@@ -39,3 +48,15 @@ def get_package_name(apktool_output_dir):
     tree = ET.parse(manifest_path)
     root = tree.getroot()
     return root.attrib['package']
+
+
+def send_email(recipient: str, body: str):
+    sender = os.environ.get('EMAIL_ADDRESS')
+    password = os.environ.get('EMAIL_PASSWORD')
+    msg = MIMEText(body)
+    msg['Subject'] = "RAML Request Status Update"
+    msg['From'] = sender
+    msg['To'] = recipient
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(sender, password)
+       smtp_server.sendmail(sender, recipient, msg.as_string())
