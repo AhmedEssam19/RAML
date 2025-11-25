89893398def03333ee07e7a47fea618b6c83be90
Ahmed <ahmed.elhlwgy@gmail.com>
2025-11-24 22:36:07 -0700

    Fix Bug

--- src/main.py
+++ src/main.py
@@ -12,7 +12,11 @@ app = FastAPI(title="FastAPI Auth Service")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=[
+        "https://raml.elhlwgy.com",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
